b3e715dda273a81553ca4c07b4f88abc3d5173db
somea82 <joemoon0526@gmail.com>
2025-01-08 10:38:51 +0900

    first commit

---
+++ src/main/java/com/example/PayAll_BE/PayAllBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PayAll_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAllBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayAllBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PayAll_BE/PayAllBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PayAll_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayAllBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
