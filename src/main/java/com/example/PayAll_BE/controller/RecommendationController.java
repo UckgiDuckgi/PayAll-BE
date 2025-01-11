09320c12b9cb40bed6ccff7d893b62fefb43429a
somea82 <joemoon0526@gmail.com>
2025-01-11 14:41:51 +0900

    🐿️feat: RecommendationController apiResult 예시적용

--- src/main/java/com/example/PayAll_BE/controller/RecommendationController.java
+++ src/main/java/com/example/PayAll_BE/controller/RecommendationController.java
@@ -8,19 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.PayAll_BE.dto.ApiResult;
 import com.example.PayAll_BE.dto.CardRecommendationResultDto;
 import com.example.PayAll_BE.service.RecommendationService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/recommendations")
 public class RecommendationController {
 
 	private final RecommendationService recommendationService;
-
-	// Constructor Injection
-	public RecommendationController(RecommendationService recommendationService) {
-		this.recommendationService = recommendationService;
-	}
 
 	/**
 	 * 사용자별 카드 추천 API
@@ -28,9 +27,9 @@ public class RecommendationController {
 	 * @return 추천 카드 및 예상 할인 결과
 	 */
 	@GetMapping("/cards")
-	public ResponseEntity<List<CardRecommendationResultDto>> recommendCards(@RequestParam Long userId) {
+	public ResponseEntity<?> recommendCards(@RequestParam Long userId) {
 		// 서비스 로직 호출
 		List<CardRecommendationResultDto> recommendations = recommendationService.getCardRecommendations(userId);
-		return ResponseEntity.ok(recommendations);
+		return ResponseEntity.ok(new ApiResult(200,"OK","추천 성공", recommendations));
 	}
 }
