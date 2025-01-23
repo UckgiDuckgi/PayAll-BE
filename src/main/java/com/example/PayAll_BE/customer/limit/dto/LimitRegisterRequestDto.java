cb976f3b5abce1a4d9b7df202d9913d4af138a0d
laputa-n <skatmdgur6365@gmail.com>
2025-01-23 16:33:49 +0900

    [test]🌱 LimitControllerTest 작성(미완)

--- src/main/java/com/example/PayAll_BE/customer/limit/dto/LimitRegisterRequestDto.java
+++ src/main/java/com/example/PayAll_BE/customer/limit/dto/LimitRegisterRequestDto.java
@@ -1,12 +1,14 @@
 package com.example.PayAll_BE.customer.limit.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class LimitRegisterRequestDto {
 	private long limitPrice;
 }
--- src/main/java/com/example/PayAll_BE/customer/limit/dto/LimitResponseDto.java
+++ src/main/java/com/example/PayAll_BE/customer/limit/dto/LimitResponseDto.java
@@ -16,9 +16,9 @@ public class LimitResponseDto {
 	private Long limitId;
 	private Long userId;
 	private Long limitPrice;
-	private long spentAmount; // 현재 소비 금액
+	private Long spentAmount; // 현재 소비 금액
 	private Long savedAmount; // 절약 금액 <<<< 반드시 로직 수정해야함 지금 일단 임시방편임
-	private long averageSpent; // 지난 3개월 평균 소비 금액
+	private Long averageSpent; // 지난 3개월 평균 소비 금액
 	private Long lastMonthLimit; // 지난달 소비 목표 금액
 	private LocalDate startDate; // 소비 목표 기간 시작 날짜
 	private LocalDate endDate; // 소비 목표 기간 종료 날짜
