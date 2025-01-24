2c8d68927342af8966e428d64e732bebc3a8f96f
insun-k <kis8294@naver.com>
2025-01-24 13:03:40 +0900

    [fix] 🧸 store, beneift 엔티티 수정

--- src/main/java/com/example/PayAll_BE/customer/benefit/Benefit.java
+++ src/main/java/com/example/PayAll_BE/customer/benefit/Benefit.java
@@ -6,9 +6,12 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +26,7 @@ public class Benefit {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "benefit_id")
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
